8907c7ef684dcd0c4d48fcaedb6ea0c155608082
MDtosif <mdtosif0mt@gmail.com>
2021-08-08 22:56:43 +0530

    O

--- credentials.js
+++ credentials.js
@@ -3,8 +3,6 @@ module.exports = {
   baseURL,
   client_id: process.env.CLIENT_ID || 'Your Google client ID',
   client_secret: process.env.CLIENT_SECRET || 'Your google secret',
-  redirect_uri: `${baseURL}/g-auth`, // set the redirect uri in you google auth
+  redirect_uri: `${baseURL}/g-auth`, // set the redirect uri in you google Oauth
   dbURI: process.env.DB_URI || 'mongodb://localhost:27017',
 };
-
-console.log(module.exports);
